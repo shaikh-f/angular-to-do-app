765cdec51bd2ad1cba2a12b54ae9513c4578aabe
Fahad Shaikh <f.shaikh@rewaatech.com>
2023-06-17 20:59:43 +0500

    CHORE: Remove unused imports.

--- to-do-app/src/app/app.module.ts
+++ to-do-app/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
-// import { ProductService } from './demo/service/product.service';
 
 @NgModule({
     declarations: [
